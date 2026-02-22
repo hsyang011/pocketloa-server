eccf364e1ef43a9f474de499b2c978da8fd0b652
hsyang011 <hsyang011@gmail.com>
2026-02-22 18:55:31 +0900

    Initial commit

---
+++ src/main/java/kr/pocketloa/server/PocketloaServerApplication.java
@@ -0,0 +1,13 @@
+package kr.pocketloa.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketloaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketloaServerApplication.class, args);
+    }
+
+}
